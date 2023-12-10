dc6d5fb16561c6dd3e764ddc882d7c1f9b3efdfd
diepb <bao-thinh.diep@etu.u-paris.fr>
2023-12-10 21:53:59 +0100

    4.1 4.2 4.4 4.6 4.8

--- src/regex.ml
+++ src/regex.ml
@@ -1,28 +1,48 @@
 open Regex_base
 
 let rec repeat n l =
-  failwith "À compléter"
+  if n <= 0 then [] 
+  else l @ repeat (n-1) l
 
 let rec expr_repeat n e =
-  failwith "À compléter"
+  if n <= 0 then Eps
+  else Concat (e, expr_repeat (n-1) e)
 
 let rec is_empty e =
   failwith "À compléter"
 
 let rec null e =
-  failwith "À compléter"
+  match e with 
+  | Eps -> true
+  | Base a -> false
+  | Joker -> false
+  | Concat (a,b) -> null a && null b
+  | Alt (a,b) -> null a || null b
+  | Star a -> true
 
 let rec is_finite e =
   failwith "À compléter"
 
 let product l1 l2 =
-  failwith "À compléter"
+  let rec aux l3 l4 =
+    match l3 with
+    | [] -> []
+    | a :: b -> List.map (fun el -> a @ el) l4 @ aux b l4
+  in aux l1 l2
 
 let enumerate alphabet e =
   failwith "À compléter"
 
 let rec alphabet_expr e =
-  failwith "À compléter"
+  let rec aux l =
+    match e with 
+    | Eps -> []
+    | Base a -> [a]
+    | Joker -> []
+    | Concat (a,b) -> alphabet_expr a @ alphabet_expr b
+    | Alt (a,b) -> alphabet_expr a @ alphabet_expr b
+    | Star a -> alphabet_expr a
+  in sort_uniq (aux e)
 
 type answer =
   Infinite | Accept | Reject
